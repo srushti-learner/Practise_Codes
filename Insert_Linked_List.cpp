ab8e38df2aa45830069730c5322314a9a417afe9
Srushti Giridhar Borate <borate_sg@ee.iitr.ac.in>
2022-06-10 11:06:40 +0530

    Add files via upload

---
+++ Insert_Linked_List.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+struct node {
+	int data;
+	node* next;
+};
+
+
+
+node* getNewNode (int data)
+{
+	node* temp = new node ();
+	temp->data=data;
+	temp->next=NULL;
+	return temp;
+}
+
+node* Insertfront (node*head,int data)
+{
+	if (head==NULL)
+	{
+		head=getNewNode(data);
+		return head;
+	}
+	node* temp=getNewNode(data);
+	temp->next=head;
+	head=temp;
+	return head;
+	
+}
+
+node* Insert (node* head,int data)
+{
+	if (head==NULL)
+	{
+		head=getNewNode(data);
+		return head;
+	}
+	node* temp=head;
+	while(temp->next !=NULL)
+	{
+	    temp=temp->next;
+	}
+	temp->next=getNewNode(data);
+	return head;
+
+
+}
+
+void Print (node* head)
+{
+	node* temp = head;
+
+	while(temp != NULL)
+	{
+		cout<<temp->data<<endl;
+		temp=temp->next;
+
+	}
+}
+
+int main()
+{
+	node* head =NULL;
+	int n,x;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>x;	
+		head=Insertfront(head,x);
+		
+	}
+
+	Print(head);
+	return 0;
+}
+
