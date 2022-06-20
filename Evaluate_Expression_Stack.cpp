4895b11eb777ada97d5cd81959f2d377d20e3998
Srushti Giridhar Borate <borate_sg@ee.iitr.ac.in>
2022-06-20 17:51:54 +0530

    Create Evaluate_Expression_Stack.cpp

---
+++ Evaluate_Expression_Stack.cpp
@@ -0,0 +1,43 @@
+int Solution::evalRPN(vector<string> &A) {
+    stack<int> s;
+    for(string x:A)
+    {
+        if(x=="+")
+        {
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            s.push(a+b);
+        }
+        else if(x=="-")
+        {
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            s.push(b-a);
+        }
+        else if(x=="*")
+        {
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            s.push(a*b);
+        }
+        else if(x=="/")
+        {
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            s.push(b/a);
+        }
+        else
+        {
+            s.push(stoi(x));
+        }
+    }
+    return s.top();
+}
