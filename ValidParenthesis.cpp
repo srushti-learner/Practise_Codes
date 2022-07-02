d2780bc7ee7dba4d3e4ea6c1f516069fb39af758
Srushti Giridhar Borate <borate_sg@ee.iitr.ac.in>
2022-07-02 17:09:55 +0530

    Create ValidParenthesis.cpp

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto x : s)
+        {
+            if(x=='(' || x=='{' || x=='[')
+            {
+                st.push(x);
+            }
+            else if(st.empty())
+            {
+                return false;
+            }
+            else if(x=='}' && st.top()=='{')
+            {
+                st.pop();
+                
+            } 
+            else if(x==')' && st.top()=='(')
+            {
+                st.pop();
+            }
+            else if(x==']' && st.top()=='[')
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
